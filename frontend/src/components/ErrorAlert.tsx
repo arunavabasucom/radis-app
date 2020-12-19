7c85dadd1ffa36cfa4c7a04e12e31edf01bf924e
Susannah Klaneček <suzil@protonmail.ch>
2020-12-19 15:52:58 -0600

    Handle a bad backend response on the frontend

---
+++ frontend/src/components/ErrorAlert.tsx
@@ -0,0 +1,25 @@
+import { Snackbar } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
+import React, { useState } from "react";
+
+interface ErrorAlertInterface {
+  message: string;
+}
+
+const ErrorAlert: React.FC<ErrorAlertInterface> = ({ message }) => {
+  const [open, setOpen] = useState<boolean>(true);
+  return (
+    <>
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        open={open}
+        onClose={() => setOpen(false)}
+        key={message}
+      >
+        <Alert severity="error">{message}</Alert>
+      </Snackbar>
+    </>
+  );
+};
+
+export default ErrorAlert;
