574c34455ca130e8cfc62caf6e579be97a3e8e20
Susannah Klaneček <suzil@protonmail.ch>
2021-01-11 21:18:27 -0600

    Remove the proxy :(

--- src/components/fields/MoleculeSelector.tsx
+++ src/components/fields/MoleculeSelector.tsx
@@ -28,7 +28,7 @@ const MoleculeSelector: React.FC<MoleculeSelectorProps> = ({
   const [input, setInput] = useState("");
 
   useEffect(() => {
-    axios.get(`/molecules`)
+    axios.get(`http://localhost:5000/molecules`)
       .then((response) => response.data)
       .then((responseData: MoleculesResponseData) =>
         setAllMolecules(responseData.molecules)
