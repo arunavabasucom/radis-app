b4196eaa32484089a081918f63cf8a78ceae7038
Susannah Klaneček <suzil@users.noreply.github.com>
2021-02-04 11:11:17 -0600

    Add storybook and basic example with MoleculeSelector (#234)
    * Add storybook and basic example with MoleculeSelector
    
    * Update preview.js

---
+++ src/components/fields/MoleculeSelector/__stories__/MoleculeSelector.stories.tsx
@@ -0,0 +1,30 @@
+import { Story, Meta } from "@storybook/react/types-6-0";
+import { MoleculeSelector, MoleculeSelectorProps } from "..";
+
+export default {
+  title: "Fields/MoleculeSelector",
+  component: MoleculeSelector,
+} as Meta;
+
+const Template: Story<MoleculeSelectorProps> = (args) => (
+  <MoleculeSelector {...args} />
+);
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  index: 0,
+  params: {
+    species: [{ molecule: "CO", mole_fraction: 0.1 }],
+    min_wavenumber_range: 1000,
+    max_wavenumber_range: 3000,
+    tgas: 400,
+    tvib: null,
+    trot: null,
+    pressure: 1.01325,
+    path_length: 1,
+    simulate_slit: false,
+  },
+  setParams: (_) => null,
+  moleculeValidationErrors: [],
+};
