1ccd8751529920e12aa64dafd4d6534ffd896d16
Erwan Pannier <erwan.pannier@gmail.com>
2021-10-18 21:49:28 +0200

    update to radis 0.10.3 (#461)
    * update to radis 0.10.3
    
    * add wstep='auto'

--- lib/radis-lambda/app.py
+++ lib/radis-lambda/app.py
@@ -36,10 +36,7 @@ def lambda_handler(event, context):
             Trot=payload["trot"],
             path_length=payload["path_length"],
             export_lines=False,
-            warnings={
-                # Do not raise error if grid too coarse. Discard once we have wstep='auto'. https://github.com/radis/radis/issues/184
-                "AccuracyError": "warn",
-            },
+            wstep='auto',
             use_cached=True,
         )
     except radis.misc.warning.EmptyDatabaseError:
