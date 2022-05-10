f10080081d621f06d9040bb7f54173c50ed0da9d
arunavabasu-03 <arunavabasuDev@gmail.com>
2022-05-10 18:53:39 +0000

    🌱 options added

---
+++ website/src/components/fields/DatabankSelector/options.ts
@@ -0,0 +1,1 @@
+export const optionsDatabank = ["HITRAN", "GEISA"];
---
+++ website/src/components/fields/DatabankSelector/options.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Box, InputLabel, MenuItem, FormControl, Select } from "@mui/material";
+
+const Options: React.FC = () => {
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          // value={age}
+          label="Age"
+          // onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+};
+export { Options };
