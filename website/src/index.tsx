aa339c41da47073b75cc7935cd414ffb9d6cabc9
Arunava Basu <arunavabasudev@gmail.com>
2022-06-21 21:39:55 +0530

    remove comments :)

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -1,22 +1,8 @@
 import React from "react";
-// import ReactDOM from "react-dom";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-// ================================================
-// deperacted rendering for the Radis app
-// ================================================
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-// ================================================
-// deperacted rendering for the Radis app
-// ================================================
 
 const cointainer = document.getElementById("root") as HTMLElement;
 const rootCointainer = ReactDOM.createRoot(cointainer);
