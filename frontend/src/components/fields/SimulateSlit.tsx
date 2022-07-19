1420d342b394841b2e5c2cb600bf6f55022fc01a
Arunava Basu <arunavabasudev@gmail.com>
2022-07-20 00:50:25 +0530

    fixed :)

--- frontend/src/components/fields/SimulateSlit.tsx
+++ frontend/src/components/fields/SimulateSlit.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
 import { Control, Controller } from "react-hook-form";
