1239e29ae3b92bf80be582f7aa40d382616d254e
Arunava Basu <arunavabasudev@gmail.com>
2022-08-02 01:32:31 +0530

    @eslint -->>`no-empty` function removed

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -5,5 +5,4 @@
 import "@testing-library/jest-dom";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-ignore
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-window.URL.createObjectURL = function () {};
+window.URL.createObjectURL = () => "";
