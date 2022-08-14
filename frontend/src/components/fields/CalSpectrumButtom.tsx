0352695ec6ce811735e493b23434917f1fc03569
Arunava Basu <arunavabasudev@gmail.com>
2022-08-14 21:30:44 +0530

    remove the size from `CALCULATE SPECTRUM button`

--- frontend/src/components/fields/CalSpectrumButtom.tsx
+++ frontend/src/components/fields/CalSpectrumButtom.tsx
@@ -3,7 +3,6 @@ import Button from "@mui/material/Button";
 
 export const CalcSpectrumButton: React.FC = () => (
   <Button
-    size="medium"
     id="calc-spectrum-button"
     disabled={false}
     variant="contained"
