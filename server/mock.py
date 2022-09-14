494626950248fc5c712d515b453fc7744612c0c6
Arunava Basu <arunavabasudev@gmail.com>
2022-09-15 00:48:55 +0530

    fixing schema && adding mock files as python `dict`

---
+++ server/mock.py
@@ -0,0 +1,3 @@
+import response as response
+import json
+print (type(json.dumps(response.response)))
--- server/test_main.py
+++ server/test_main.py
@@ -2,22 +2,37 @@ import json
 import pytest
 from httpx import AsyncClient
 from main import app  
-from json.decoder import JSONDecodeError
-from fastapi.testclient import TestClient
+import response as response
 
-client = TestClient(app)
+response_dict = response.response
+request_body = {
+    "species": [
+        {
+            "molecule": "CO",
+            "mole_fraction": 0.2
+        }
+    ],
+    "mode": "absorbance",
+    "database": "hitran",
+    "tgas": 300,
+    "min_wavenumber_range": 1900,
+    "max_wavenumber_range": 2300,
+    "pressure": 1.01325,
+    "path_length": 11,
+    "use_simulate_slit": True,
+    "simulate_slit": 5,
+    "wavelength_units": "1/u.cm",
+    "path_length_units": "u.km",
+    "pressure_units": "cds.atm"
+}
 
 # testing for calculate_spectrum endpoint
 @pytest.mark.anyio
 async def test_calculate_spectrum():
-    async with AsyncClient(app=app, base_url="https://api.radis.app") as ac:
-         with open('./mocks/request.json', 'r') as f:
-              client_request = json.load(f)
-              request_json = json.dumps(client_request)
-         response = await ac.post("/calculate-spectrum",json=request_json)
+    async with AsyncClient(app=app, base_url="https://api.radis.app") as ac:  
+        # response_json = json.dumps(request_body)
+        response = await ac.post("/calculate-spectrum",json=request_body,headers={"Content-Type": "application/json"})
          # open the response.json 
-         with open('./mocks/response.json', 'r') as f:
-              server_response = json.load(f)
-              response_json = json.dumps(server_response)
-    assert response.status_code == 200
+        response_json = json.dumps(response_dict)
+#     assert response.status_code == 200
     assert response.json() == response_json
