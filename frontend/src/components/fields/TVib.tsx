74771cac9ee8ebe9696628d433c169cc42b4525e
Arunava Basu <arunavabasudev@gmail.com>
2024-01-21 20:51:35 +0530

    refactor: adding tvib

--- frontend/src/components/fields/TVib.tsx
+++ frontend/src/components/fields/TVib.tsx
@@ -2,46 +2,45 @@ import Input from "@mui/joy/Input";
 import FormControl from "@mui/joy/FormControl";
 import FormLabel from "@mui/joy/FormLabel";
 import FormHelperText from "@mui/joy/FormHelperText";
-import React, { ReactNode } from "react";
-import InputAdornment from "@mui/material/InputAdornment";
-import { Control, Controller } from "react-hook-form";
-import { FormValues } from "../types";
-interface TVibProps {
-  control: Control<FormValues>;
-}
-export const TVib: React.FC<TVibProps> = ({ control }) => (
-  <Controller
-    render={({ field, formState }) => (
-      <FormControl>
-        <FormLabel>TVib</FormLabel>
-        <Input
-          {...field}
-          {...formState}
-          id="tvib-input"
-          type="number"
-          onChange={field.onChange}
-          value={field.value}
-          error={!!formState.errors?.tvib}
-          endDecorator={<InputAdornment position="end">K</InputAdornment>}
-          onKeyPress={(event: any) => {
-            if (event?.key === "-" || event?.key === "+") {
-              event.preventDefault();
-            }
-          }}
-        />
-        {formState.errors?.tvib ? (
-          <FormHelperText
-            sx={{
-              color: "red",
+import { Controller, useFormContext } from "react-hook-form";
+
+
+export const TVib: React.FC = () => {
+  const { control } = useFormContext();
+  
+  return (
+    <Controller
+      name="tvib"
+      control={control}
+      defaultValue={300}
+      render={({ field, fieldState }) => (
+        <FormControl>
+          <FormLabel>TVib</FormLabel>
+          <Input
+            {...field}
+            id="tvib-input"
+            type="number"
+            onChange={field.onChange}
+            value={field.value}
+            error={!!fieldState.error}
+            endDecorator={"k"}
+            onKeyPress={(event: any) => {
+              if (event?.key === "-" || event?.key === "+") {
+                event.preventDefault();
+              }
             }}
-          >
-            {formState.errors?.tvib?.message}
-          </FormHelperText>
-        ) : null}
-      </FormControl>
-    )}
-    name="tvib"
-    control={control}
-    defaultValue={300}
-  />
-);
+          />
+          {fieldState.error ? (
+            <FormHelperText
+              sx={{
+                color: "red",
+              }}
+            >
+              {fieldState.error.message}
+            </FormHelperText>
+          ) : null}
+        </FormControl>
+      )}
+    />
+  );
+};
